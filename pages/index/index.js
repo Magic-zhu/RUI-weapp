da9dfed050d5600119f908d663357c86268d0e4d
magic-zhu <18817313320@163.com>
2020-03-06 11:01:40 +0800

    feat:save

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,36 +4,40 @@ const app = getApp()
 
 Page({
   data: {
-    navBarOptions:{
-        barTitleText:"基础组件",
-        frontColor:"#000000",
+    navBarOptions: {
+      barTitleText: "基础组件",
+      frontColor: "#000000",
     },
-    list:[
-        {
-            name:"Button",
-            path:"/pages/Button_Ex/index"
-        },
-        {
-            name:"Cell",
-            path:"/pages/Cell_Ex/index"
-        },
-        {
-            name:"Popup",
-            path:"/pages/Popup_Ex/index"
-        },
-        {
-            name:"Curtain",
-            path:"/pages/Curtain_Ex/index"
-        },
+    list: [
+      {
+        name: "Button",
+        path: "/pages/Button_Ex/index"
+      },
+      {
+        name: "Cell",
+        path: "/pages/Cell_Ex/index"
+      },
+      {
+        name: "Popup",
+        path: "/pages/Popup_Ex/index"
+      },
+      {
+        name: "Curtain",
+        path: "/pages/Curtain_Ex/index"
+      },
+      {
+        name: "Image",
+        path: "/pages/Image_Ex/index"
+      }
     ]
   },
-  onLoad(){
+  onLoad() {
 
   },
-  jump(e){
-    wx.$.click(()=>{
-        let {index} = wx.$.get(e);
-        wx.$.navigate(this.data.list[index].path);
+  jump(e) {
+    wx.$.click(() => {
+      let { index } = wx.$.get(e);
+      wx.$.navigate(this.data.list[index].path);
     })
   }
 })
