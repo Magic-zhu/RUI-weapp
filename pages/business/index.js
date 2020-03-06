b9a71498ee9f121691e130b16ad77fa88f5094cf
magic-zhu <18817313320@163.com>
2020-03-06 13:39:58 +0800

    feat:save

---
+++ pages/business/index.js
@@ -0,0 +1,43 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    navBarOptions: {
+      barTitleText: "业务组件",
+      frontColor: "#000000",
+    },
+    list: [
+      {
+        name: "Button",
+        path: "/pages/Button_Ex/index"
+      },
+      {
+        name: "Cell",
+        path: "/pages/Cell_Ex/index"
+      },
+      {
+        name: "Popup",
+        path: "/pages/Popup_Ex/index"
+      },
+      {
+        name: "Curtain",
+        path: "/pages/Curtain_Ex/index"
+      },
+      {
+        name: "Image",
+        path: "/pages/Image_Ex/index"
+      }
+    ]
+  },
+  onLoad() {
+
+  },
+  jump(e) {
+    wx.$.click(() => {
+      let { index } = wx.$.get(e);
+      wx.$.navigate(this.data.list[index].path);
+    })
+  }
+})
