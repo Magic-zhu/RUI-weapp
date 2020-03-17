71efc824eb9d89c91f3a10c68e854c95c342aa1f
magic-zhu <18817313320@163.com>
2020-03-17 14:42:49 +0800

    feat:更新

---
+++ pages/about/index.js
@@ -0,0 +1,16 @@
+//Page Object
+Page({
+    data: {
+        navBarOptions: {
+            barTitleText: "关于",
+            frontColor: "#000000",
+          },
+    },
+    //options(Object)
+    onLoad: function(options){
+        
+    },
+    onShow: function(){
+        
+    },
+});
