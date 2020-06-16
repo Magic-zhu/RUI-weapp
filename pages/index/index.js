2a944cb0281ad1b61f2ad043761e397177ae5908
magic-zhu <18817313320@163.com>
2020-06-16 17:41:20 +0800

    save

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,49 +3,53 @@
 const app = getApp()
 
 Page({
-  data: {
-    navBarOptions: {
-      barTitleText: "基础组件",
-      frontColor: "#000000",
+    data: {
+        navBarOptions: {
+            barTitleText: "基础组件",
+            frontColor: "#000000",
+        },
+        list: [
+            {
+                name: "Canvas库",
+                path: "/pages/Canvas_Ex/index"
+            },
+            {
+                name: "Animation库",
+                path: ""
+            },
+            {
+                name: "Button",
+                path: "/pages/Button_Ex/index"
+            },
+            {
+                name: "Cell",
+                path: "/pages/Cell_Ex/index"
+            },
+            {
+                name: "Popup",
+                path: "/pages/Popup_Ex/index"
+            },
+            {
+                name: "Curtain",
+                path: "/pages/Curtain_Ex/index"
+            },
+            {
+                name: "Image",
+                path: "/pages/Image_Ex/index"
+            },
+            {
+                name: "Icon",
+                path: "/pages/Icon_Ex/index"
+            },
+        ]
     },
-    list: [
-      {
-        name: "Button",
-        path: "/pages/Button_Ex/index"
-      },
-      {
-        name: "Cell",
-        path: "/pages/Cell_Ex/index"
-      },
-      {
-        name: "Popup",
-        path: "/pages/Popup_Ex/index"
-      },
-      {
-        name: "Curtain",
-        path: "/pages/Curtain_Ex/index"
-      },
-      {
-        name: "Image",
-        path: "/pages/Image_Ex/index"
-      },
-      {
-        name:"Icon",
-        path:"/pages/Icon_Ex/index"
-      },
-      {
-        name:"Canvas",
-        path:"/pages/Canvas_Ex/index"
-      },
-    ]
-  },
-  onLoad() {
+    onLoad() {
 
-  },
-  jump(e) {
-    wx.$.click(() => {
-      let { index } = wx.$.get(e);
-      wx.$.navigate(this.data.list[index].path);
-    })
-  }
+    },
+    jump(e) {
+        wx.$.click(() => {
+            let { index } = wx.$.get(e);
+            wx.$.navigate(this.data.list[index].path);
+        })
+    }
 })
